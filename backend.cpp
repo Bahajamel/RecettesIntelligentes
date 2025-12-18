edb22fef69e4b698892b667219b5e691b5a0d8df
BahaJamelDev <bahaeddine.jamel@ensi-uma.tn>
2025-12-18 10:17:09 +0100

    merging

---
+++ backend.cpp
@@ -0,0 +1,54 @@
+#include "backend.h"
+
+Backend::Backend(QObject *parent)
+    : QObject(parent),
+
+    // 1️⃣ Database
+    m_dbManager("app.db"),
+
+    // 2️⃣ DAO
+    m_recetteDAO(m_dbManager.database()),
+    m_ingredientDAO(m_dbManager.database()),
+    m_riDAO(m_dbManager.database()),
+    m_instructionDAO(m_dbManager.database()),
+
+    // 3️⃣ Services
+    m_ingredientService(m_ingredientDAO, m_riDAO),
+    m_instructionService(m_instructionDAO),
+    m_recetteService(m_recetteDAO, m_riDAO, m_ingredientService, m_instructionService),
+
+    // 4️⃣ Models
+    m_recetteModel(m_recetteService)
+{
+    m_dbManager.open();
+    chargerRecettes() ;
+}
+
+RecetteTableModel* Backend::recetteModel()
+{
+    return &m_recetteModel;
+}
+
+void Backend::chargerRecettes()
+{
+    QList<Recette> recettes = m_recetteService.listerRecettes();
+    m_recetteModel.setRecettes(recettes);
+}
+int Backend::creerRecette(const QString &titre, const QString &description)
+{
+    int id = m_recetteService.creerRecette(titre, description);
+    if (id < 0) {
+        emit erreur("Impossible de créer la recette");
+        return 0;
+    }
+    return id ;
+    chargerRecettes(); // rafraîchir la vue
+}
+
+void Backend::ajouterIngredientARecette(const int &id , const QString &Ingredient , const float &qte , const QString &unite) {
+    Ingredient ing = static_cast<Ingredient>(Ingredient) ;
+    UNITE unit = static_cast<UNITE> unite ;
+
+
+}
+
---
+++ backend.h
@@ -0,0 +1,58 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <QObject>
+#include "dataHeader/databasemanager.h"
+
+// DAO
+#include "daoHeader/ingredientDAO.h"
+#include "daoHeader/recetteDAO.h"
+#include "daoHeader/recetteingredientdao.h"
+#include "daoHeader/instructionDAO.h"
+
+// Services
+#include "serviceHeader/ingredientService.h"
+#include "serviceHeader/instructionService.h"
+#include "serviceHeader/recetteService.h"
+
+// Models
+#include "modelsHeader/recettemodel.h"
+
+
+class Backend : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Backend(QObject *parent = nullptr);
+
+    RecetteTableModel* recetteModel();
+
+public slots:
+    void chargerRecettes();
+    int creerRecette(const QString &titre, const QString &description);
+    void ajouterIngredientARecette(const int &id , const QString &nom , const float &qte , const QString &unite);
+
+signals:
+    void erreur(const QString &message);
+
+private:
+    DatabaseManager m_dbManager;
+
+    // DAO
+    RecetteDAO m_recetteDAO;
+    IngredientDAO m_ingredientDAO;
+    RecetteIngredientDAO m_riDAO;
+    InstructionDAO m_instructionDAO;
+
+    // Services
+    IngredientService m_ingredientService;
+    InstructionService m_instructionService;
+    RecetteService m_recetteService;
+
+    // Models
+    RecetteTableModel m_recetteModel;
+};
+
+#endif
+
+
